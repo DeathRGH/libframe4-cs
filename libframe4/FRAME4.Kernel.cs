9869483d439a19d1ea18320c3f7fe9a41bdbf8f5
DeathRGH <sebihanel@gmail.com>
2025-01-28 00:52:01 +0100

    added KernelVmMap

--- libframe4/FRAME4.Kernel.cs
+++ libframe4/FRAME4.Kernel.cs
@@ -1,60 +1,100 @@
 using System;
+using System.Net.Sockets;
 
 namespace libframe4
 {
-	public partial class FRAME4
-	{
-		// kernel
-		// packet sizes
+    public partial class FRAME4
+    {
+        // kernel
+        // packet sizes
 
-		// send size
-		private const int CMD_KERN_READ_PACKET_SIZE = 12;
-		private const int CMD_KERN_WRITE_PACKET_SIZE = 12;
+        // send size
+        private const int CMD_KERN_READ_PACKET_SIZE = 12;
+        private const int CMD_KERN_WRITE_PACKET_SIZE = 12;
 
-		// receive size
-		private const int KERN_BASE_SIZE = 8;
+        // receive size
+        private const int KERN_BASE_SIZE = 8;
+        private const int KERN_MAP_ENTRY_SIZE = 58;
 
-		/// <summary>
-		/// Get kernel base address
-		/// </summary>
-		/// <returns></returns>
-		public ulong KernelBase()
-		{
-			CheckConnected();
+        /// <summary>
+        /// Get kernel base address
+        /// </summary>
+        /// <returns></returns>
+        public ulong KernelBase()
+        {
+            CheckConnected();
 
-			SendCMDPacket(CMDS.CMD_KERN_BASE, 0);
-			CheckStatus();
-			return BitConverter.ToUInt64(ReceiveData(KERN_BASE_SIZE), 0);
-		}
+            SendCMDPacket(CMDS.CMD_KERN_BASE, 0);
+            CheckStatus();
+            return BitConverter.ToUInt64(ReceiveData(KERN_BASE_SIZE), 0);
+        }
 
-		/// <summary>
-		/// Read memory from kernel
-		/// </summary>
-		/// <param name="address">Memory address</param>
-		/// <param name="length">Data length</param>
-		/// <returns></returns>
-		public byte[] KernelReadMemory(ulong address, int length)
-		{
-			CheckConnected();
+        /// <summary>
+        /// Read memory from kernel
+        /// </summary>
+        /// <param name="address">Memory address</param>
+        /// <param name="length">Data length</param>
+        /// <returns></returns>
+        public byte[] KernelReadMemory(ulong address, int length)
+        {
+            CheckConnected();
 
-			SendCMDPacket(CMDS.CMD_KERN_READ, CMD_KERN_READ_PACKET_SIZE, address, length);
-			CheckStatus();
-			return ReceiveData(length);
-		}
+            SendCMDPacket(CMDS.CMD_KERN_READ, CMD_KERN_READ_PACKET_SIZE, address, length);
+            CheckStatus();
+            return ReceiveData(length);
+        }
 
-		/// <summary>
-		/// Write memory in kernel
-		/// </summary>
-		/// <param name="address">Memory address</param>
-		/// <param name="data">Data</param>
-		public void KernelWriteMemory(ulong address, byte[] data)
-		{
-			CheckConnected();
+        /// <summary>
+        /// Write memory in kernel
+        /// </summary>
+        /// <param name="address">Memory address</param>
+        /// <param name="data">Data</param>
+        public void KernelWriteMemory(ulong address, byte[] data)
+        {
+            CheckConnected();
 
-			SendCMDPacket(CMDS.CMD_KERN_WRITE, CMD_KERN_WRITE_PACKET_SIZE, address, data.Length);
-			CheckStatus();
-			SendData(data, data.Length);
-			CheckStatus();
-		}
-	}
+            SendCMDPacket(CMDS.CMD_KERN_WRITE, CMD_KERN_WRITE_PACKET_SIZE, address, data.Length);
+            CheckStatus();
+            SendData(data, data.Length);
+            CheckStatus();
+        }
+
+        /// <summary>
+        /// Get the kernel memory map
+        /// </summary>
+        /// <returns></returns>
+        public KernelVmMap GetKernelVmMap()
+        {
+            CheckConnected();
+
+            SendCMDPacket(CMDS.CMD_KERN_VM_MAP, 0);
+            CheckStatus();
+
+            // recv count
+            byte[] bnumber = new byte[4];
+            sock.Receive(bnumber, 4, SocketFlags.None);
+            int number = BitConverter.ToInt32(bnumber, 0);
+
+            // recv data
+            byte[] data = ReceiveData(number * KERN_MAP_ENTRY_SIZE);
+
+            // parse data
+            KernelMemoryEntry[] entries = new KernelMemoryEntry[number];
+            for (int i = 0; i < number; i++)
+            {
+                int offset = i * KERN_MAP_ENTRY_SIZE;
+                entries[i] = new KernelMemoryEntry
+                {
+                    name = ConvertASCII(data, offset),
+                    start = BitConverter.ToUInt64(data, offset + 32),
+                    end = BitConverter.ToUInt64(data, offset + 40),
+                    offset = BitConverter.ToUInt64(data, offset + 48),
+                    prot = BitConverter.ToUInt16(data, offset + 56)
+                };
+
+            }
+
+            return new KernelVmMap(entries);
+        }
+    }
 }
